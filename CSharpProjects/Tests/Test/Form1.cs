ecc6f27f9733252dc233f2be573bf1073585e6c3
Jeremy Pratt <jerpratt1@gmail.com>
2023-04-27 15:23:17 -0400

    Basic program

---
+++ CSharpProjects/Tests/Test/Form1.cs
@@ -0,0 +1,15 @@
+namespace Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Welcome to my program";
+        }
+    }
+}
